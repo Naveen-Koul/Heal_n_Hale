1d2381ac568d5e6bbc5b2f265fffc057bcad61e0
Naveen Koul <koul1.naveen@gmail.com@gmail.com>
2020-11-14 00:04:48 -0500

    added validation

---
+++ contactus.js
@@ -0,0 +1,12 @@
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    var y = document.forms["myForm"]["lname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    if (y == "") {
+      alert(" last Name must be filled out");
+      return false;
+    }
+  }
